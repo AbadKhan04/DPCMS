e1d1e40c3ab98d00b6c8d2799fb833e295c5093d
AbadKhan04 <pafkiet04@gmail.com>
2023-08-22 23:09:36 +0500

    Applied Command Pattern on User

--- DPCMS/User.cs
+++ DPCMS/User.cs
@@ -20,47 +20,72 @@ namespace DPCMS
             InitializeComponent();
         }
 
+        public interface ICommand
+        {
+            void Execute();
+        }
+
+        public class BookRideCommand : ICommand
+        {
+            private readonly DPCMS_CONNECTION _connection;
+            private readonly string _pickup;
+            private readonly string _destination;
+            private readonly string _fare;
+            private readonly string _ridetype;
+
+            public BookRideCommand(DPCMS_CONNECTION connection, string pickup, string destination, string fare, string ridetype)
+            {
+                _connection = connection;
+                _pickup = pickup;
+                _destination = destination;
+                _fare = fare;
+                _ridetype = ridetype;
+            }
+
+            public void Execute()
+            {
+                try
+                {
+                    _connection.connect_open();
+                    Console.WriteLine("Connected to the database-user.");
+
+                    string sql = "INSERT INTO booking (pickup, destination, fare, ridetype) " +
+                                 "VALUES (@pickup, @destination, @fare, @ridetype)";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, _connection.con))
+                    {
+                        cmd.Parameters.AddWithValue("@pickup", _pickup);
+                        cmd.Parameters.AddWithValue("@destination", _destination);
+                        cmd.Parameters.AddWithValue("@fare", _fare);
+                        cmd.Parameters.AddWithValue("@ridetype", _ridetype);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Ride Book successful!");
+                            map map = new map();
+                            map.Show();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Booking failed.");
+                        }
+                    }
+                    _connection.connect_close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+
         private void btnride_Click(object sender, EventArgs e)
         {
-            try
-            {
-                connection.connect_open();
-                Console.WriteLine("Connected to the database-user.");
-
-                string pickup = txtpick.Text;
-                string destination = txtdestination.Text;
-                string fare = txtfare.Text;
-                string ridetype = comboBox1.SelectedItem.ToString();
-
-                string sql = "INSERT INTO booking (pickup, destination, fare, ridetype) " +
-                             "VALUES (@pickup, @destination, @fare, @ridetype)";
-
-                using (SqlCommand cmd = new SqlCommand(sql, connection.con))
-                {
-                    cmd.Parameters.AddWithValue("@pickup", pickup);
-                    cmd.Parameters.AddWithValue("@destination", destination);
-                    cmd.Parameters.AddWithValue("@fare", fare);
-                    cmd.Parameters.AddWithValue("@ridetype", ridetype);
-
-                    int rowsAffected = cmd.ExecuteNonQuery();
-
-                    if (rowsAffected > 0)
-                    {
-                        MessageBox.Show("Ride Book successful!");
-                        map map = new map();
-                        map.Show();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Booking failed.");
-                    }
-                }
-                connection.connect_close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            ICommand bookRideCommand = new BookRideCommand(connection, txtpick.Text, txtdestination.Text, txtfare.Text, comboBox1.SelectedItem.ToString());
+            bookRideCommand.Execute();
         }
         private void btn_logout_Click(object sender, EventArgs e)
         {
